89b487a9191fe0077209bc74e54670882d2048e4
edw-tnx <edw-toni@hotmail.com>
2019-09-12 17:00:43 -0500

    comments to frontController

--- controller/FrontController.php
+++ controller/FrontController.php
@@ -4,7 +4,13 @@ class FrontController {
 
     public function __construct() {     
     }
-
+    /*
+    * Routeador
+    * Recibe parámetros c=> contorlador y a=>acción
+    * completa el nombre del contorlador que se envía mediante la URL
+        * index => IndexController
+    * Crea Objeto del controlador y ejecuta la acción
+    */
     public function route() {
         $this->sessionStart();
         $controller = (isset($_REQUEST['c'])) ? $_REQUEST['c'] : 'Index';
@@ -16,6 +22,7 @@ class FrontController {
         $controller = new $controller; // creacion del objeto controlador
         $controller->$action(); //llamada a la funcion del controlador (action) que se va a ejecutar    
     }
+    // Inicia sesión en caso de que no exista
     public function sessionStart(){
         if (!isset($_SESSION)) {
             session_start();
